6079ccbcc8a45e3b7ddf801281af2d931f9f99d6
Christoph Borgolte <christoph.borgolte@bayard-consulting.com>
2016-09-30 10:59:15 +0200

    Added argv parser.
    This one seems not to be the best choice. Or i was not able to find a
    mandatory flag for options.

--- bin/tree_node_parser.js
+++ bin/tree_node_parser.js
@@ -2,20 +2,57 @@
 
 "use strict";
 
+let argv = require('argv');
 let parser = require('../libs/tree-node-parser/parser.js');
 
+argv.option([
+  {
+    name: 'input',
+    short: 'i',
+    type: 'path',
+    description: 'The path to the underlying tree node csv file.',
+    example: "'script --input=fixtures/nodes.csv' or 'script -i fixtures/nodes.csv'"
+  },
+  {
+    name: 'start-node',
+    short: 's',
+    type: 'string',
+    description: 'The node from which to start collecting leaf nodes.',
+    example: "'script --start-nodes=fruit' or 'script -s fruit'"
+  },
+  {
+    required: true,  // XXX: Normally option parsers should support this flag!
+    name: 'find-path-to',
+    short: 'p',
+    type: 'string',
+    description: 'The node to find the path to, starting from the root node.',
+    example: "'script --find-path-to=lemon' or 'script -p lemon'"
+  },
+  {
+    name: 'nodes-with-n-children',
+    short: 'n',
+    type: 'int',
+    description: 'Find all nodes with this amount of children'
+  }
+]);
+
+let args = argv.run();
+let options = args.options;
+
+let filename = options['input'];
+let nodename = options['find-path-to'];
+let startNode = options['start-node'];
+let amountOfChildren = options['nodes-with-n-children'];
 
 function error(msg) {
   console.error(msg)
   process.exit(1)
 }
 
-if (process.argv.length != 4) {
-  error('usage: ' + process.argv[1] + ' {nodes.csv} {nodename}');
+if (![filename, nodename, startNode, amountOfChildren].every(v=> v !== undefined)) {
+  argv.help();
+  process.exit(9);
 }
-
-let filename = process.argv[2]
-let nodename = process.argv[3]
 
 function callback(nodes, edges, adjacencyList) {
   // guard: make sure, given nodename exists
